46ac5d2a1cbbf3b38d05812d509092dd018fb5de
MaximZhubia <mazim.zh2001@gmail.com>
2022-06-03 21:41:50 +0300

    Initial commit

---
+++ ATMchik/Migrations/20211029175959_first.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ATMchik.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BankAccountNumber = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    CardNumber = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    PvvHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CvvHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Owner = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+        }
+    }
+}
